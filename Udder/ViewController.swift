b14b4e121074dec2b8180e8419e8736d56ad4ac0
Denis Jahic <denisjahic@deniss-macbook-pro.local>
2019-01-29 20:20:59 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Denis Jahic on 29.01.19.
+//  Copyright © 2019 Denis Jahic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
